6b873862f9f3e70ea71c33b91edd653a6ed58eec
Aditya <aditya.jhajharia.24@gmail.com>
2026-08-20 13:07:14 +0530

    chore: remove python refactor scripts

--- fix_app_logout.py
+++
@@ -1,8 +0,0 @@
-with open('src/App.tsx', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace("import { initCaseEngine, backfillCaseHealthMemory }", "import { initCaseEngine, clearCaseEngineCache, backfillCaseHealthMemory }")
-content = content.replace("window.dispatchEvent(new Event('hc_logout'));", "window.dispatchEvent(new Event('hc_logout'));\n          clearCaseEngineCache();")
-
-with open('src/App.tsx', 'w', encoding='utf-8') as f:
-    f.write(content)
